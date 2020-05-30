b8971cfcb3150c721e42ce5239195bd3d160de3a
Sergey Mysov <s.mysov@list.ru>
2020-05-31 00:22:26 +0400

    fix(navigation): fix mobile-menu and added annimation

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,21 +1,21 @@
 (function () {
   const hamburger = document.querySelector(".hamburger-menu");
   const icon = document.querySelector(".hamburger-menu__icon");
-  const navigation = document.querySelector(".site-list");
+  const navigation = document.querySelector(".main-navigation");
   const header = document.querySelector(".header");
 
   function hideNav() {
     icon.classList.add("hamburger-menu__icon--burger");
     icon.classList.remove("hamburger-menu__icon--cross");
     header.classList.remove("header--background");
-    navigation.classList.remove("site-list--active");
+    navigation.classList.remove("main-navigation--active");
   }
 
   function showNav() {
     icon.classList.remove("hamburger-menu__icon--burger");
     icon.classList.add("hamburger-menu__icon--cross");
     header.classList.add("header--background");
-    navigation.classList.add("site-list--active");
+    navigation.classList.add("main-navigation--active");
   }
 
   hamburger.addEventListener("click", (e) => {
